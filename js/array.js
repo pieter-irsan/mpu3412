4b3a463d41f297c996cdc3bd7ffdd6f8a6ed8b26
pieter-irsan <pieterirsan.pi@gmail.com>
2022-06-20 01:53:21 +0700

    Save progress 20/06/2022

---
+++ js/array.js
@@ -0,0 +1,204 @@
+const warga1 = [
+    ['M. Aman', 'Miko', 'Pras', 'Luthfi', 'Luhung', 'Ibran', 'Maulana'],
+    ['Hidayat', 'Rafid', 'Narji', 'Maman', 'Galuh', 'Ian', 'Dodo'],
+    ['Kemal', 'Rosinton', 'Ananta', 'Bimo', 'Eman', 'Anggi', 'Chandra'],
+    ['Arta', 'Ikhsan', 'Adit', 'Aslijan', 'Kardi', 'Edi', 'Asmuni'], 
+    ['Iskandar', 'Santoso', 'Reza', 'Galiono', 'Hartana', 'Yanto', 'Garang'],
+    ['Jefri', 'Omar', 'Ikin', 'Ajimin', 'Oskar', 'Nasim', 'Joko'], 
+    ['Atta', 'Pram', 'Yosef', 'Kambali', 'Algo', 'Lulut', 'Balijan'],
+    ['Panji', 'Ismail', 'Adi', 'Johan', 'Cayadi', 'Radit', 'Opan'], 
+    ['Jaya', 'Dadi', 'Kasiran', 'Pandu', 'Iwan', 'Sutoyo', 'Rusli']
+    // ['Mohammad Gamani', 'Jatmiko', 'Cawuk Prasasta', 'Luthfi Wasita', 'Luhung Mahendra', 'Ibrani Permadi', 'Respati Maulana'],
+    // ['Kenari Hidayat', 'Rafid Siregar', 'Narji Adriansyah', 'Maman Prabowo', 'Galuh Hutagalung', 'Ian Utama', 'Dodo Widodo'],
+    // ['Kemal Sitorus', 'Rosinton Kawaca', 'Pranata Santoso', 'Kala Andestyo', 'Eman Zulkarnain', 'Umaya Anggriawan', 'Chandra Saptono'],
+    // ['Arta Situmorang', 'Ikhsan Sitompul', 'Aditya Chairy', 'Aslijan Simanjuntak', 'Kardi Dongoran', 'Edi Jailani', 'Asmuni Ramadan'], 
+    // ['Lamar Iskandar', 'Santoso', 'Muhammad Reza', 'Galiono Januar', 'Hartana Marbun', 'Warsita Hidayanto', 'Garang Tarihoran'],
+    // ['Jefri Rajata', 'Omar Najmudin', 'Ikin Sudrajat', 'Ajimin Kusumo', 'Oskar Latupono', 'Nasim Waskita', 'Joko Hutasoit'], 
+    // ['Prima Panggabean', 'Pramuda Wardhono', 'Yosef Wacana', 'Kambali Prakasa', 'Unggul Wibisono', 'Lulut Latupono', 'Balijan Tampubolon'],
+    // ['Panji Uwais', 'Ismail Prayoga', 'Baktiadi Ardianto', 'Johan Pratama', 'Cayadi Hartono', 'Radit Pranowo', 'Opan Haryanto'], 
+    // ['Jayadi Rajasa', 'Dadi Suryadi', 'Kasiran Megantara', 'Pandu', 'Muhammad Irwandi', 'Sutoyo', 'Rusli Sapardi']
+];
+
+const warga2 = [
+    [
+        "Luhung",
+        "Ibran",
+        "M. Aman",
+        "Luthfi",
+        "Miko",
+        "Pras",
+        "Maulana"
+    ],
+    [
+        "Hidayat",
+        "Narji",
+        "Rafid",
+        "Dodo",
+        "Ian",
+        "Maman",
+        "Galuh"
+    ],
+    [
+        "Eman",
+        "Kemal",
+        "Rosinton",
+        "Ananta",
+        "Anggi",
+        "Chandra",
+        "Bimo"
+    ],
+    [
+        "Aslijan",
+        "Arta",
+        "Ikhsan",
+        "Asmuni",
+        "Kardi",
+        "Adit",
+        "Edi"
+    ],
+    [
+        "Galiono",
+        "Garang",
+        "Reza",
+        "Yanto",
+        "Hartana",
+        "Iskandar",
+        "Santoso"
+    ],
+    [
+        "Joko",
+        "Omar",
+        "Ajimin",
+        "Jefri",
+        "Oskar",
+        "Nasim",
+        "Ikin"
+    ],
+    [
+        "Algo",
+        "Lulut",
+        "Kambali",
+        "Yosef",
+        "Pram",
+        "Balijan",
+        "Atta"
+    ],
+    [
+        "Opan",
+        "Cayadi",
+        "Johan",
+        "Radit",
+        "Adi",
+        "Ismail",
+        "Panji"
+    ],
+    [
+        "Iwan",
+        "Dadi",
+        "Jaya",
+        "Pandu",
+        "Rusli",
+        "Kasiran",
+        "Sutoyo"
+    ]
+]
+
+const warga3 = [
+    [
+        "Miko",
+        "Ibran",
+        "Pras",
+        "Luthfi",
+        "Maulana",
+        "Luhung",
+        "M. Aman"
+    ],
+    [
+        "Narji",
+        "Ian",
+        "Hidayat",
+        "Rafid",
+        "Galuh",
+        "Maman",
+        "Dodo"
+    ],
+    [
+        "Bimo",
+        "Chandra",
+        "Ananta",
+        "Rosinton",
+        "Kemal",
+        "Anggi",
+        "Eman"
+    ],
+    [
+        "Aslijan",
+        "Adit",
+        "Edi",
+        "Ikhsan",
+        "Kardi",
+        "Arta",
+        "Asmuni"
+    ],
+    [
+        "Iskandar",
+        "Santoso",
+        "Galiono",
+        "Hartana",
+        "Reza",
+        "Yanto",
+        "Garang"
+    ],
+    [
+        "Jefri",
+        "Nasim",
+        "Omar",
+        "Joko",
+        "Ikin",
+        "Ajimin",
+        "Oskar"
+    ],
+    [
+        "Balijan",
+        "Pram",
+        "Kambali",
+        "Algo",
+        "Yosef",
+        "Lulut",
+        "Atta"
+    ],
+    [
+        "Johan",
+        "Panji",
+        "Ismail",
+        "Radit",
+        "Adi",
+        "Opan",
+        "Cayadi"
+    ],
+    [
+        "Kasiran",
+        "Rusli",
+        "Jaya",
+        "Pandu",
+        "Sutoyo",
+        "Dadi",
+        "Iwan"
+    ]
+]
+
+const warga4 = ''
+
+const warga5 = ''
+
+const warga6 = ''
+
+const warga7 = ''
+
+const warga8 = ''
+
+const warga9 = ''
+
+const warga10 = ''
+
+module.exports = { warga1, warga2, warga3, warga4, warga5, warga6, warga7, warga8, warga9, warga10 };
--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,62 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+const arr = require('./array');
 
+function populateTable(table, data) {
+    data.forEach((d, i) => {
+        const row = table.insertRow(i);
+        Object.keys(d).forEach((k, j) => {
+            const cell = row.insertCell(j);
+            cell.innerHTML = d[k];
+        });
+        table.appendChild(row);
+    });
+}
+
+const tabel = document.querySelector('#jadwal tbody');
+
+populateTable(tabel, arr.warga1);
+
+// Shuffle two-dimensional array
+function fisherYates(arr) {
+    let i = arr.length, j, tempi, tempj;
+    if (i === 0) return false;
+    while (--i) {
+        j = Math.floor(Math.random() * (i + 1));
+        tempi = arr[i];
+        tempj = arr[j];
+        arr[i] = tempj;
+        arr[j] = tempi;
+    }
+    return arr;
+}
+
+Date.prototype.getWeek = function() {
+    let january1st = new Date(this.getFullYear(), 0, 1);
+    return Math.ceil((((this - january1st) / 86400000) + january1st.getDay() + 1) / 7);
+}
+
+let weekNumber = (new Date()).getWeek();
+                                                let data = $.each(warga1, function(i) { fisherYates(this) });console.log(data);
+if (weekNumber % 2 == 0) populateTable(tabel, warga2);
+if (weekNumber % 3 == 0) populateTable(tabel, warga3);
+if (weekNumber % 4 == 0) populateTable(tabel, warga4);
+if (weekNumber % 5 == 0) populateTable(tabel, warga5);
+if (weekNumber % 6 == 0) populateTable(tabel, warga6);
+if (weekNumber % 7 == 0) populateTable(tabel, warga7);
+if (weekNumber % 8 == 0) populateTable(tabel, warga8);
+if (weekNumber % 9 == 0) populateTable(tabel, warga9);
+if (weekNumber % 10 == 0) populateTable(tabel, warga10);
+
+
+// Show modal after registration form submit
+$('form').on('submit', function (e) {
+    e.preventDefault();
+    $('#modal').modal('show');
+})
+
+window.addEventListener('DOMContentLoaded', event => {
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
@@ -30,5 +84,23 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+});
 
+// Manual table shuffle 
+document.getElementById("shuffleButton").addEventListener("click", function () {
+    let table = document.querySelector(`#jadwal tbody`);
+    let data = $.each(warga, function(i) { fisherYates(this) });
+
+    $('#anggota').find("tr:gt(0)").remove();
+    const jadwal = document.querySelector('#jadwal > tbody ');
+    jadwal.firstElementChild.remove();
+    
+    data.forEach((d, i) => {
+        const row = table.insertRow(i);
+        Object.keys(d).forEach((k, j) => {
+            const cell = row.insertCell(j);
+            cell.innerHTML = d[k];
+        });
+        table.appendChild(row);
+    });
 });
